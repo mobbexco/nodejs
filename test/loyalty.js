0d59892f3d09017ceb1e3ac4f99d03535275a9ac
ezekiel <grosfeldezekel@gmail.com>
2020-09-29 15:40:30 -0300

    file creation

---
+++ test/loyalty.js
@@ -0,0 +1,6 @@
+const chai = require('chai');
+const chaiPromise = require('chai-as-promised')
+const assert = chai.assert;
+const loyalty = require('../lib/resources/loyalty')
+
+chai.use(chaiPromise)
