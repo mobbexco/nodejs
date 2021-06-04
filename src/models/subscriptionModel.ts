ad854e9a680446c121cf0b86159ecf597b14b6ee
Ezekiel Grosfeld <ezekiel.grosfeld@mobbex.com>
2021-06-04 09:33:11 -0300

    Adding test to subscription

--- src/models/subscriptionModel.ts
+++ src/models/subscriptionModel.ts
@@ -1,33 +1,34 @@
-import { ValidationSchema } from "fastest-validator";
+import { ValidationSchema } from 'fastest-validator';
 
 const schema: ValidationSchema = {
-  total: "number",
-  currency: "string",
-  name: "string",
-  description: "string",
-  type: {
-    type: "string",
-    enum: ["dynamic", "manual"],
-  },
-  interval: {
-    type: "string",
-    enum: ["7d", "15d", "1m", "2m", "3m", "6m", "1y"],
-  },
-  trial: "number|optional",
-  limit: "number",
-  webhook: {
-    type: "string",
-    pattern: "^(http)",
-  },
-  return_url: {
-    type: "string",
-    pattern: "^(http)",
-  },
-  features: {
-    type: "array",
-    optional: true,
-    items: "string",
-  },
+	total: 'number',
+	currency: 'string',
+	name: 'string',
+	test: 'boolean|optional',
+	description: 'string',
+	type: {
+		type: 'string',
+		enum: ['dynamic', 'manual']
+	},
+	interval: {
+		type: 'string',
+		enum: ['7d', '15d', '1m', '2m', '3m', '6m', '1y']
+	},
+	trial: 'number|optional',
+	limit: 'number',
+	webhook: {
+		type: 'string',
+		pattern: '^(http)'
+	},
+	return_url: {
+		type: 'string',
+		pattern: '^(http)'
+	},
+	features: {
+		type: 'array',
+		optional: true,
+		items: 'string'
+	}
 };
 
 export default schema;
--- test/subscription.ts
+++ test/subscription.ts
@@ -1,61 +1,81 @@
-import * as chai from "chai";
-import * as chaiAsPromised from "chai-as-promised";
-import { assert } from "chai";
-import subscription from "../src/resources/subscription";
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { assert } from 'chai';
+import subscription from '../src/resources/subscription';
 
 chai.use(chaiAsPromised);
 
-const unauthorizedError = "Request failed with status code 401";
+const unauthorizedError = 'Request failed with status code 401';
 
-describe("Subscription Module", () => {
-  const id = "mv4vuUGYG";
-  describe("Successfully handle subscrptions", () => {
-    it("Should make request to create a new subscription", () => {
-      return assert.isRejected(
-        subscription.create({
-          total: 200.0,
-          currency: "ARS",
-          name: "Prueba",
-          description: "Prueba",
-          type: "dynamic",
-          interval: "1m",
-          trial: 1,
-          limit: 0,
-          webhook: "http://webhook.com",
-          return_url: "http://return_url.com",
-          features: ["accept_no_funds"],
-        }),
-        unauthorizedError
-      );
-    });
+describe('Subscription Module', () => {
+	const id = 'mv4vuUGYG';
+	describe('Successfully handle subscrptions', () => {
+		it('Should make request to create a new subscription', () => {
+			return assert.isRejected(
+				subscription.create({
+					total: 200.0,
+					currency: 'ARS',
+					name: 'Prueba',
+					description: 'Prueba',
+					type: 'dynamic',
+					interval: '1m',
+					trial: 1,
+					limit: 0,
+					webhook: 'http://webhook.com',
+					return_url: 'http://return_url.com',
+					features: ['accept_no_funds']
+				}),
+				unauthorizedError
+			);
+		});
 
-    it("Should make request to edit the subscription", () => {
-      return assert.isRejected(
-        subscription.edit(id, {
-          total: 300.0,
-        }),
-        unauthorizedError
-      );
-    });
+		it('Should create a test suscription', () => {
+			return assert.isRejected(
+				subscription.create({
+					total: 200.0,
+					test: true,
+					currency: 'ARS',
+					name: 'Prueba',
+					description: 'Prueba',
+					type: 'dynamic',
+					interval: '1m',
+					trial: 1,
+					limit: 0,
+					webhook: 'http://webhook.com',
+					return_url: 'http://return_url.com',
+					features: ['accept_no_funds']
+				}),
+				unauthorizedError
+			);
+		});
 
-    it("Should make request to get all the subscriptions", () => {
-      return assert.isRejected(subscription.all(), unauthorizedError);
-    });
+		it('Should make request to edit the subscription', () => {
+			return assert.isRejected(
+				subscription.edit(id, {
+					total: 300.0
+				}),
+				unauthorizedError
+			);
+		});
 
-    it("Should make request to get the second page of subscriptions", () => {
-      return assert.isRejected(subscription.all(2), unauthorizedError);
-    });
+		it('Should make request to get all the subscriptions', () => {
+			return assert.isRejected(subscription.all(), unauthorizedError);
+		});
 
-    it("Should make request to find the subscription", () => {
-      return assert.isRejected(subscription.find(id), unauthorizedError);
-    });
+		it('Should make request to get the second page of subscriptions', () => {
+			return assert.isRejected(subscription.all(2), unauthorizedError);
+		});
 
-    it("Should make request to active the subscription", () => {
-      return assert.isRejected(subscription.activate(id), unauthorizedError);
-    });
+		it('Should make request to find the subscription', () => {
+			return assert.isRejected(subscription.find(id), unauthorizedError);
+		});
 
-    it("Should make request to delete the subscription", () => {
-      return assert.isRejected(subscription.delete(id), unauthorizedError);
-    });
-  });
+		it('Should make request to active the subscription', () => {
+			return assert.isRejected(subscription.activate(id), unauthorizedError);
+		});
+
+		it('Should make request to delete the subscription', () => {
+			return assert.isRejected(subscription.delete(id), unauthorizedError);
+		});
+	});
 });
